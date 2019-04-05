b9f259ec3c78181b5aa8ce7336b8f163379b6e6c
EliotFerragni <eliot.ferragni@epfl.ch>
2019-04-05 14:14:19 +0200

    -Deleted files specific to PD-Buddy_Firmware

--- boards/PD_BUDDY_SINK/board.mk
+++
@@ -1,9 +0,0 @@
-# List of all the board related files.
-BOARDSRC = $(CHIBIOS)/../boards/PD_BUDDY_SINK/board.c
-
-# Required include directories
-BOARDINC = $(CHIBIOS)/../boards/PD_BUDDY_SINK $(CHIBIOS)/../src
-
-# Shared variables
-ALLCSRC += $(BOARDSRC)
-ALLINC  += $(BOARDINC)
