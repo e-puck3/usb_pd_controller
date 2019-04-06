4877b95dcab55488d28c4bbc931c1695c611168a
EliotFerragni <eliot.ferragni@epfl.ch>
2019-04-06 20:53:08 +0200

    -Added pdbs structure definitions and defines to pdb.h

--- pdblib/include/pdb.h
+++ pdblib/include/pdb.h
@@ -32,6 +32,55 @@
 #define PDB_LIB_MINOR 1
 #define PDB_LIB_PATCH 0
 
+/* Status for configuration structures.  EMPTY indicates that the struct is
+ * ready to be written, including a status update to VALID.  Once the struct is
+ * no longer needed, the status is updated to INVALID.  Erasing the flash page
+ * resets all structures to EMPTY. */
+#define PDBS_CONFIG_STATUS_INVALID 0x0000
+#define PDBS_CONFIG_STATUS_VALID 0xBEEF
+#define PDBS_CONFIG_STATUS_EMPTY 0xFFFF
+
+/* Flags for configuration structures. */
+/* GiveBack supported */
+#define PDBS_CONFIG_FLAGS_GIVEBACK (1 << 0)
+/* Variable and battery PDOs preferred (FIXME: not implemented) */
+#define PDBS_CONFIG_FLAGS_VAR_BAT (1 << 1)
+/* High voltages preferred */
+#define PDBS_CONFIG_FLAGS_HV_PREFERRED (1 << 2)
+/* Current definition type */
+#define PDBS_CONFIG_FLAGS_CURRENT_DEFN_SHIFT 3
+#define PDBS_CONFIG_FLAGS_CURRENT_DEFN (0x3 << PDBS_CONFIG_FLAGS_CURRENT_DEFN_SHIFT)
+#define PDBS_CONFIG_FLAGS_CURRENT_DEFN_I (0 << PDBS_CONFIG_FLAGS_CURRENT_DEFN_SHIFT)
+#define PDBS_CONFIG_FLAGS_CURRENT_DEFN_P (1 << PDBS_CONFIG_FLAGS_CURRENT_DEFN_SHIFT)
+#define PDBS_CONFIG_FLAGS_CURRENT_DEFN_R (2 << PDBS_CONFIG_FLAGS_CURRENT_DEFN_SHIFT)
+
+/*
+ * PD Buddy Sink configuration structure
+ */
+struct pdbs_config {
+    /* Status halfword, used to indicate which config objects can be written to
+     * and which one is valid. */
+    uint16_t status;
+    /* Flags halfword for miscellaneous small fields. */
+    uint16_t flags;
+    /* Preferred voltage, in millivolts. */
+    uint16_t v;
+    /* Union for specifying how much current to request. */
+    union {
+        /* Required current, in centiamperes. */
+        uint16_t i;
+        /* Required power, in centiwatts. */
+        uint16_t p;
+        /* Value of resistive load, in centiohms. */
+        uint16_t r;
+    };
+    /* Lower end of voltage range, in millivolts. */
+    uint16_t vmin;
+    /* Upper end of voltage range, in millivolts. */
+    uint16_t vmax;
+    /* Extra bytes reserved for future use. */
+    uint16_t _reserved[2];
+} __attribute__((packed));
 
 /*
  * Structure for one USB port's PD Buddy firmware library configuration
@@ -47,6 +96,9 @@ struct pdb_config {
     struct pdb_dpm_callbacks dpm;
     /* Pointer to port-specific DPM data */
     void *dpm_data;
+
+    /* Pointer to PD config */
+    struct pdbs_config* pd_config;
 
     /* Automatically initialized fields */
     /* Policy Engine thread and related variables */
